42c58b339312b0d1475b27a695c5032b3d0d1ade
Villalobos <diegovillalobos397@gmail.com>
2020-07-20 18:42:05 +0200

    Added wrapper for redis streams Fixed dependency having ^ in package.json

--- src/utils/redis-wrapper/src/index.js
+++ src/utils/redis-wrapper/src/index.js
@@ -1,9 +1,11 @@
 import Geo from './geo';
 import Hash from './hash';
 import SortedSet from './sortedSet';
+import Stream from './stream';
 
 export default {
 	Geo,
 	Hash,
-	SortedSet
+	SortedSet,
+	Stream
 };
